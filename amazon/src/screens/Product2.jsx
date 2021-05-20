4d0fec200c75473938256cc26ab2ee764e7c6dd7
Andri Rafti <andrirafti@Andris-MacBook-Air.local>
2021-05-20 12:00:40 -0400

    update

--- amazon/src/screens/Product2.jsx
+++ amazon/src/screens/Product2.jsx
@@ -78,7 +78,7 @@ const Product2 = (props) => {
           
     <div className="rowSide">
     
-          <input list="product-list" className="search" placeholder="search" type="text" value={search} onChange={(e) => setSearch(e.target.value)}>
+          <input list="product-list" className="search" placeholder="search" type="text" >
             <datalist id="product-list">
               <option value={queryProduct.map(val=>val)}></option>
             </datalist>
