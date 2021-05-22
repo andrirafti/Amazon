823b98994767eee8e563c04487e8c5fae22e4098
Andri Rafti <andrirafti@Andris-MacBook-Air.local>
2021-05-22 11:25:38 -0400

    m

--- amazon/src/screens/Product2.jsx
+++ amazon/src/screens/Product2.jsx
@@ -62,13 +62,6 @@ const Product2 = (props) => {
 
   }
   
-            <input list="car-list" type="text">
-              <datalist id="car-list">
-                <option value="Ferrari"/>
-                  <option value="Mustang"/>
-                    <option value="Porsche"/>
-              </datalist>
-            </input>
   
   return (
     isAuthenticated && (
@@ -82,6 +75,13 @@ const Product2 = (props) => {
           <option value="LowPrice" >Low To High</option>
           </select>
           <div>
+            <input list="car-list" type="text">
+              <datalist id="car-list">
+                <option value="Ferrari"/>
+                  <option value="Mustang"/>
+                    <option value="Porsche"/>
+              </datalist>
+            </input>
           </div>
           </label>
          
