1178a518c52b7984212c89a6bb8cd20efc16c784
Andri Rafti <andrirafti@Andris-MacBook-Air.local>
2021-05-22 11:39:50 -0400

    update

--- amazon/src/screens/Product2.jsx
+++ amazon/src/screens/Product2.jsx
@@ -26,8 +26,8 @@ const Product2 = (props) => {
    
     if (e.target.value == "LowPrice") setQueryProduct2(LowHigh);
     if (e.target.value == "HighPrice") setQueryProduct2(HighLow);
-    if (e.target.value = "Ferrari") alert("Wow its a ferrari!");
-    if(e.target.value="Ferrari") alert("Wow its a Porsche!")
+    if (e.target.value == "Ferrari") alert("Wow its a ferrari!");
+    if(e.target.value=="Porsche") alert("Wow its a Porsche!")
  
   }
   
